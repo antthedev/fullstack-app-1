1f66749ebee9c88db3fc30cdb8c1a27f0678728a
Antonie Smith <61763868+antthedev@users.noreply.github.com>
2022-11-07 00:14:54 -0500

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://antTheDev:Cocopuffs12@cluster0.hnilr5a.mongodb.net/book?retryWrites=true&w=majority', 
+    'dbName': 'book'
+};
---
+++ public/main.js
@@ -0,0 +1,55 @@
+var star = document.getElementsByClassName("fa-star");
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(star).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        let stars = 5
+        if (this.classList.contains('star4')){
+         stars = 4
+        }else if (this.classList.contains('star3')){
+          stars = 3
+        } else if (this.classList.contains('star2')){
+          stars = 2
+        }else if (this.classList.contains('star1')){
+          stars = 1
+        }
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'stars': stars
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
